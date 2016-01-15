0415818cd55b1ef6f277d13e336e3c92107198df
dabit3 <dabit3@gmail.com>
2016-01-15 08:44:25 -0600

    Updated to babel 6

--- webpack.config.js
+++ webpack.config.js
@@ -4,18 +4,15 @@ module.exports = {
    filename: "bundle.js"
  },
  module: {
-   preLoaders: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        loader: 'jshint-loader'
-      }
-   ],
+   
    loaders: [
      {
        test: [/\.js$/, /\.es6$/],
        exclude: /node_modules/,
-       loader: 'babel-loader'
+       loader: 'babel-loader',
+       query: {
+         presets: ['react', 'es2015'] 
+       }
      }
    ]
  },
